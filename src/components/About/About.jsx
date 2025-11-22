224bcdf0557e4029f55e0dd882277e259fbd8fb1
Narayan Gawas <gawasnarayan766@gmail.com>
2025-11-22 21:20:43 +0530

    Fix download attribute for resume link

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -41,7 +41,7 @@ const About = () => {
           </p>
           {/* resume button */}
           <a href="/new_resume(1).pdf"
-              download
+              
           target='_blank'
           rel='noopener noreferrer'
           className='inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105'
